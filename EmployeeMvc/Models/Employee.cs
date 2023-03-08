6e8fc7ed51630ebe8b1fb73b5c95f0ca82111976
Isam <isamov2012@hotmail.com>
2023-03-08 09:39:14 +0100

    make Employee class

---
+++ EmployeeMvc/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeMvc.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		[Required (ErrorMessage ="Enter a name.")]
+		public string Name { get; set; }
+		[Required(ErrorMessage ="Enter an email.")]
+		[EmailAddress]
+		[Display(Name="E-mail")]
+        public string Email { get; set; }
+    }
+}
