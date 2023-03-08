adf23f6c3d47eaaa1aa08150aa66cfddb705459e
Lisann <lisann.dahlen@web.de>
2023-03-08 10:39:55 +0100

    obligatoriska delen

---
+++ EmployeeMvc/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMvc.Controllers
+{
+	public class EmployeesController : Controller
+	{
+		static DataService dataService = new DataService();
+
+		[HttpGet("")]
+		public IActionResult Index()
+		{
+			Employee[] model = dataService.GetAll();
+			return View(model);
+		}
+		
+		[HttpGet("/create")]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+        [HttpPost("/create")]
+        public IActionResult Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            dataService.Add(employee);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- EmployeeMvc/Models/Employee.cs
+++ EmployeeMvc/Models/Employee.cs
@@ -2,14 +2,14 @@
 
 namespace EmployeeMvc.Models
 {
-	public class Employee
-	{
-		public int Id { get; set; }
-		[Required (ErrorMessage ="Enter a name.")]
-		public string Name { get; set; }
-		[Required(ErrorMessage ="Enter an email.")]
-		[EmailAddress]
-		[Display(Name="E-mail")]
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Enter a name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Enter an email.")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Your Email is not valid.")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
     }
 }
