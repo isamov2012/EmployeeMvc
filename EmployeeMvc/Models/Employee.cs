9290bf88bd962454e9cbebc322cffb1562048256
Isam <isamov2012@hotmail.com>
2023-03-08 12:41:40 +0100

    delete required attribute

--- EmployeeMvc/Models/Employee.cs
+++ EmployeeMvc/Models/Employee.cs
@@ -7,7 +7,6 @@ namespace EmployeeMvc.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Enter a name.")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Enter an email.")]
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Your Email is not valid.")]
         [Display(Name = "E-mail")]
         public string Email { get; set; }
